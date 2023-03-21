744104f848d7654c428cb1490978aee550bca50d
Michael O'Rawe <michaelorawe@btinternet.com>
2023-03-21 18:23:51 +0000

    Obstructed movement

--- stochastic2.py
+++ stochastic2.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tree_gen
 
-(nu,nx,N,lf,lr,ts) = (2,4,40,1,1,0.1)       #nu = Number of inputs, nx = number of states
+(nu,nx,N,lf,lr,ts) = (2,4,26,1,1,0.1)       #nu = Number of inputs, nx = number of states
 ltot = lf+lr
-(Rcar, Robj) = (1,1)
-Rmin = 0.5
+(Rcar, Robj) = (0.5,0.5)
+Rmin = 0.2
+(obs_x, obs_y) = (0.5,3)
 
 (xref, yref, psiref, vref) = (1,1,0,0)   #Target
 (q,qpsi,qbeta,r_v,r_beta,qN,qPsiN,qBetaN) = (10.0, 0.1, 0.1, 1,1, 200, 2,2)
@@ -27,7 +28,7 @@ tree = tree_gen.MarkovChainScenarioTreeFactory(p, v_tree, N_tree, tau).create()
 u = cs.SX.sym('u', nu*tree.num_nonleaf_nodes)
 z0 = cs.SX.sym('z0', nx)
 
-(x,y,psi,v) = (z0[0], z0[1], z0[2], z0[3])
+(x,y,v, psi) = (z0[0], z0[1], z0[2], z0[3])
 
 cost = tree.probability_of_node(0)*(q*((x-xref)**2+(y-yref)**2)+qpsi*(psi-psiref)**2+qbeta*(v-vref)**2)
 cost += tree.probability_of_node(0)*(r_v*u[0]**2+r_beta*u[1]**2)
@@ -37,6 +38,7 @@ z_sequence = [None]*tree.num_nonleaf_nodes
 z_sequence[0] = z0
 
 c=0
+f2=[]
 for i in range(1,tree.num_nonleaf_nodes):       #Looping through all non-leaf nodes
     idx_anc = tree.ancestor_of(i) 
     prob_i = tree.probability_of_node(i)
@@ -57,10 +59,11 @@ for i in range(1,tree.num_nonleaf_nodes):       #Looping through all non-leaf no
     cost += prob_i*(q*((x_current-xref)**2+(y_current-yref)**2)+qpsi*(psi_current-psiref)**2+qbeta*(v_current-vref)**2)
     cost += prob_i*(r_v*u[0]**2+r_beta*u[1]**2)
     # cost += prob_i*(1*cs.dot(u_current, u_current))
+    f2 = cs.vertcat(f2, Rmin**2-(obs_x-x_current)**2-(obs_y-y_current)**2)
 
     z_sequence[i]=cs.vertcat(x_current,y_current,v_current,psi_current)
 
-bounds = og.constraints.BallInf(radius = 1)
+bounds = og.constraints.BallInf(radius = 1.8)
 
 for i in range(tree.num_nodes):
     tree.set_data_at_node(i, {"pos": [1,1]})
@@ -68,8 +71,8 @@ for i in range(tree.num_nodes):
 # f2  =cs.vertcat(cs.fmax(0.0,Rmin**2-(0-z_sequence[:][0])**2-(2-z_sequence[:][1])**2))
 
 problem = og.builder.Problem(u, z0,cost)\
-        .with_constraints(bounds)
-        # .with_penalty_constraints(f2)  
+        .with_constraints(bounds)\
+        .with_penalty_constraints(f2)  
   
 build_config = og.config.BuildConfiguration()\
     .with_build_directory("basic_optimizer")\
@@ -77,11 +80,15 @@ build_config = og.config.BuildConfiguration()\
     .with_tcp_interface_config()
 
 meta = og.config.OptimizerMeta()\
-    .with_optimizer_name("bicycle")
+    .with_optimizer_name("bicycle")                            
 
 solver_config = og.config.SolverConfiguration()\
-    .with_tolerance(1e-3)\
-    .with_initial_tolerance(1e-3)
+    .with_tolerance(1e-4)\
+    .with_initial_tolerance(1e-3)\
+    .with_preconditioning()\
+    .with_penalty_weight_update_factor(1.4)\
+    .with_max_inner_iterations(2000)\
+    .with_max_outer_iterations(60)
 
 builder = og.builder.OpEnOptimizerBuilder(problem,
                                             meta,
@@ -95,7 +102,7 @@ builder = og.builder.OpEnOptimizerBuilder(problem,
 mng = og.tcp.OptimizerTcpManager('basic_optimizer/bicycle')
 mng.start()
 
-init_pos = [-1.0, 4.0, 0.0, 0.0]
+init_pos = [-1.0, 5.0, 0.0, 0.0]
 
 mng.ping()
 solution = mng.call(init_pos)
@@ -149,14 +156,26 @@ xy = x_states[1:nx*N:nx]
 xpsi = x_states[3:nx*N:nx]
 xbeta = x_states[4:nx*N:nx]
 
+figure, axes = plt.subplots()
 plt.plot(xx, xy, '-o')
+# plt.scatter(0,1,s=10000, facecolors='none', edgecolors='blue')
+circle = plt.Circle((obs_x,obs_y),Robj,color='r')
+axes.set_aspect(1)
+axes.add_artist(circle)
+
 plt.xlabel("X Position")
 plt.ylabel("Y Position")
 plt.show()
 
+# fig = plt.figure()
+# circle = plt.Circle((0,1), radius=0.5, color='green')
+# fig.add_artist(circle)
+# plt.show()
+
 print(f"Solution exit status "+str(solution["exit_status"]))
 print(f"Solution penalty "+str(solution["penalty"]))
 print(f"Solution time "+str(solution["solve_time_ms"]))
-print(f"Iterations "+str(solution["num_inner_iterations"]))
+print(f"Inner iterations "+str(solution["num_inner_iterations"]))
+print(f"Outer iterations "+str(solution["num_outer_iterations"]))
+print(f"F2 norm " +str(solution["f2_norm"]) )
 print(f"Cost "+str(solution["cost"]))
-
